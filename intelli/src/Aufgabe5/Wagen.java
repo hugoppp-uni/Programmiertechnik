a18bb171c960ee5de01be49c498b3201815d7446
acs521 <hugo.protsch@haw-hamburg.de>
2019-11-23 19:01:20 +0100

    Variablennamen geändert, beim abhaengen() wird referenz jetzt entfernt

--- intelli/src/Aufgabe5/Wagen.java
+++ intelli/src/Aufgabe5/Wagen.java
@@ -1,6 +1,11 @@
 package Aufgabe5;
 
 public class Wagen {
+    /**
+     * Referenz auf den nächsten Wagen.
+     * Wenn 'naechsterWagen = null' ist kein Wagen angehängt
+     */
+
     Wagen naechsterWagen = null;
     private String id;
 
--- intelli/src/Aufgabe5/Zug.java
+++ intelli/src/Aufgabe5/Zug.java
@@ -1,6 +1,10 @@
 package Aufgabe5;
 
 public class Zug {
+    /**
+     * Referenz auf den ersten Wagen des Zuges
+     * Wenn 'ersterWagen = null' ist kein Wagen angehängt
+     */
     private Wagen ersterWagen = null;
 
     /**
@@ -14,11 +18,7 @@ public class Zug {
             ersterWagen = neuerWagen;
         } else {
             //sonst wird an den letzten Wagen der neue rangehängt
-            Wagen letzerWagen = durchWagenItterieren(null);
-            /*Wagen aktuellerWagen = ersterWagen;
-            while (aktuellerWagen.naechsterWagen != null) {
-                aktuellerWagen = aktuellerWagen.naechsterWagen;
-            }*/
+            Wagen letzerWagen = findeVorgängerVon(null);
             letzerWagen.naechsterWagen = neuerWagen;
         }
     }
@@ -32,20 +32,25 @@ public class Zug {
     public void abhaengen(Wagen wagen) {
         //1. Fall: ersten Wagen abhängen
         if (ersterWagen == wagen) {
-            ersterWagen = ersterWagen.naechsterWagen;
+            Wagen wagenZuEntfernen = ersterWagen;
+            ersterWagen = wagenZuEntfernen.naechsterWagen;
+            wagenZuEntfernen.naechsterWagen = null;
+            
         } else {
             //2. Fall: irgendein anderen Wagen abhängen;
-            Wagen gesuchterWagen = durchWagenItterieren(wagen);
-            gesuchterWagen.naechsterWagen = gesuchterWagen.naechsterWagen.naechsterWagen;
+            //z.B Zug-gesuchterWagen-wagenZuEntfernen-wagen3
+            Wagen gesuchterWagen = findeVorgängerVon(wagen);
+            Wagen wagenZuEnfernen = gesuchterWagen.naechsterWagen;
+            gesuchterWagen.naechsterWagen = wagenZuEnfernen.naechsterWagen;
         }
 
     }
-
+    
     /**
      * @param wagen nächsterWagen des gesuchten Wagens
      * @return der Wagen, der vor dem eingegebenen Wagen hägt, zB: Zug-Wagen1-Ausgabe-Eingabe-Wagen4
      */
-    private Wagen durchWagenItterieren(Wagen wagen) {
+    private Wagen findeVorgängerVon(Wagen wagen) {
         Wagen aktuellerWagen = ersterWagen;
         while (aktuellerWagen.naechsterWagen != wagen) {
             aktuellerWagen = aktuellerWagen.naechsterWagen;
